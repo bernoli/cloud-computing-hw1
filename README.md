858886f4ea436df2529986cb2ec6f645fdd685c0
yoav kaplan <yoav.kaplan@gmail.com>
2022-04-27 10:36:14 +0300

    change charge calculation and added better error handling

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# cloud-computing-hw1
+# Cloud Computing HW1
+
+### Purpose 
+This assignment focus on the process of automating the infrastructure setup and application deployment on AWS. 
+The application is very simple and far from being robust.
+
+### Description 
+Simple parking management system with 2 HTTP endpoints:
+* POST /entry?plate=123-123-123&parkingLot=382 
+  * Returns ticket id
+
+* POST /exit?ticketId=1234
+  * Returns the license plate, total parked time, the parking lot id and the charge (based on 15 minutes increments).
+
+
+### Setup on AWS
+Use setup.sh to provision an Ubuntu 20.04 ec2 instance (free-tier) and installs the packages and infrastructure required to run the application.
+* Redis server
+* Python Flask 
+* The app code (very basic and simple, no persistency and other real world app)
+
+Before you run this script you need to make sure you have [AWS CLI](https://aws.amazon.com/cli/) installed on your local machine.
+
--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 import json
 import redis
 import time
+import math
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -17,19 +18,20 @@ def heath_check():
 def enter_parking():
 	entry_timestamp = time.time()
 	args = request.args
-	try:
-		plate = args.get("plate")
-		parking_lot = args.get("parkingLot")
+	plate = args.get("plate")
+	parking_lot = args.get("parkingLot")
+	if (plate is not None and len(plate.strip())) and (parking_lot is not None  and len(parking_lot.strip())):
 		entry_data = json.dumps(
 		{'enter_timestamp': entry_timestamp, 'plate_number': plate, 'parking_lot': parking_lot})
 		ticket_id = f"{plate}_{parking_lot}" #car plate & parkinglot combination is universally unique 
+		
 		if redis_db.exists(ticket_id):
 			return jsonify({'error': 'car is already parking in the requests parking lot.'})
 
 		redis_db.set(ticket_id, entry_data)
 
 		return ticket_id
-	except ValueError:
+	else:
 		return jsonify({'error': 'invalid request please verify your parameters.'})
 
 
@@ -37,24 +39,25 @@ def enter_parking():
 def exit_parking():
 	exit_timestamp = time.time()
 	ticket_id = request.args.get("ticketId")
+	
+	if ticket_id is None or (not len(ticket_id.strip())):
+		return jsonify({'error': 'invalid request please verify your parameters.'})
+	
 	parking_data = redis_db.get(ticket_id)
 	redis_db.delete(ticket_id)
 	
-	if parking_data is None:
+	if parking_data is None or (not len(parking_data.strip())):
 		return jsonify({'error':'ticket id not found.'})
-	
-	redis_db.delete(ticket_id)
-	
+
 	parking_info = json.loads(parking_data)
 	entry_timestamp = parking_info['enter_timestamp']
 	plate = parking_info['plate_number']
 	parking_lot = parking_info['parking_lot']
 	total_seconds = exit_timestamp - entry_timestamp
 	total_minutes = total_seconds / 60
-	charge_units = total_minutes / minutes_in_hour
+	charge_units = math.ceil(total_minutes / minutes_in_hour)
 	total_charge = charge_units * fee
-	result = {'plate':plate,'total_parking_time_in_minutes':total_minutes,'charge_units':charge_units,'parking_lot':parking_lot, 'total_amount':total_charge}
+	result = {'plate' : plate, 'total_parking_time_in_minutes':total_minutes, 'parking_lot' : parking_lot, 'total_charge':total_charge}
 	
 	return jsonify(result)
-
 
